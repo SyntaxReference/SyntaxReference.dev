babacb06bb34757fe6a302633689d3ccc0351262
Leonnardo Verol <6124410+LeonnardoVerol@users.noreply.github.com>
2020-05-17 17:36:01 -0300

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,8 +3,9 @@ module.exports = {
 	description: 'A syntax reference guide for multiple programming languages',
 	themeConfig: {
 		repo: 'https://github.com/LeonnardoVerol/Programming-Languages-Syntax-Reference',
-		repoLabel: 'Help us improve this page! Contribute on Github',
+		repoLabel: 'Contribute on Github!',
 		sidebar: require('./sidebar'),
 		nav: require('./nav'),
+		searchMaxSuggestions: 20,
 	}
 }
