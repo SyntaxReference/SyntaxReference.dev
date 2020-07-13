a660555bbd74da3015ddb36231bb33a584f0478b
Leonnardo Verol <6124410+LeonnardoVerol@users.noreply.github.com>
2020-07-13 13:32:52 -0300

    Fix Logo Name

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
 	title: 'Syntax Reference',
 	description: 'A syntax reference guide for multiple programming languages and others',
 	themeConfig: {
-		logo: '/assets/img/logo.png',
+		logo: '/assets/img/Logo.png',
 		repo: 'https://github.com/SyntaxReference/SyntaxReference.dev',
 		repoLabel: 'Contribute on Github!',
 		sidebar: require('./sidebar/sidebar'),
